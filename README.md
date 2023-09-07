d7010e3498712a80aec35c5ae863e2b02b39f442
Rasif Sahl <rasifsahl@Rasifs-MacBook-Pro.local>
2023-09-07 07:34:19 +0530

    front end changes

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # express-react-authentication
 
-* [Back-End](express-backend/BACKEND.md)
+## Back End
+
+[Back-End](express-backend/BACKEND.md)
 
 If you need all the authentication in the same file `"main": "index.js",` to `server.js`
 
-* [Front-End](react-frontend/FRONTEND.md)
+## Front End
+[Front-End](react-frontend/FRONTEND.md)
---
+++ react-frontend/src/request/apiCall.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:4000'; // Replace with your API base URL
+
+const apiCall = async (method, url, data, token = null) => {
+  try {
+    const config = {
+      method,
+      url: `${BASE_URL}${url}`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    if (data) {
+      config.data = data;
+    }
+
+    const response = await axios(config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default apiCall;
